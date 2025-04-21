336e235a767fe96ca7578f23c12959a5206f9be4
Kwabena Ampofo <amp.kwabena@gmail.com>
2025-04-21 13:35:30 -0400

    implement crud with todos array

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,87 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-
-// import routes
-import todoRoutes from "./routes/todo.routes";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
-const app: Express = express();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
 
 
-// app middlewear
 app.use(express.json());
 
-// Routing middleware
-app.use("/api/todos", todoRoutes);
+interface Todo {
+  id: number;
+  content: any;
+  status: string;
+}
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+const todos: Todo[] = [];
+
+let id = 1;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port: ${port}`);
+// Create
+app.post("/api/todos", (req: Request, res: Response) => {
+  const todo: Todo = {
+    "id": id,
+    "content": req.body["content"],
+    "status": "todo"
+  };
+
+  todos.push(todo);
+  id += 1;
+
+  res.status(200).send("Todo added successfully");
+});
+
+
+// Read
+app.get("/api/todos", (req: Request, res: Response) => {
+  res.send(JSON.stringify(todos));
+});
+
+app.get("/api/todos/:id", (req: Request, res: Response) => {
+  const id = req.params["id"];
+
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i]["id"].toString() == id) {
+      res.status(200).send(todos[i]);
+    }
+  }
+  // res.send("no todo with that id found");
+});
+
+// Update
+app.put("/api/todos/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const updatedContent = { ...req.body };
+
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i]["id"].toString() == id) {
+      todos[i] = { ...todos[i], ...updatedContent };
+      console.log(todos[i]);
+    }
+  }
+
+  res.send(`Todo with id ${id} successfully updated`);
+});
+
+// Delete
+app.delete("/api/todos/:id", (req: Request, res: Response) => {
+  const id = req.params["id"];
+  todos.filter((todo) => todo["id"].toString() != id);
+
+  res.send(`Todo with id ${id} successfully deleted`);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`server listening on port: ${PORT}`);
 });
